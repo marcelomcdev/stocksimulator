aa2e12e0a3b31ad766b3a58ae8ac078c57527600
Marcelo Castro <marcelo.castro@sysmap.com.br>
2021-02-16 16:29:58 -0300

    Add auth controller and some configurations and fixes

--- Backend/StockSimulator.Application/Controllers/AuthController.cs
+++ Backend/StockSimulator.Application/Controllers/AuthController.cs
@@ -2,12 +2,16 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using StockSimulator.Application.Security;
 using StockSimulator.Application.ViewModels;
 using StockSimulator.CrossCutting.Configuration;
 using StockSimulator.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace StockSimulator.Application.Controllers
@@ -52,12 +56,45 @@ namespace StockSimulator.Application.Controllers
         [HttpPost("sign_in")]
         public async Task<ActionResult> Login(LoginUserViewModel loginUser)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.Values.SelectMany(er => er.Errors));
 
+            var result = await _signInManager.PasswordSignInAsync(loginUser.UserName, loginUser.Password, false, false);
+
+            if (result.Succeeded)
+                return Ok(await GenerateJwt(loginUser.UserName));
+
+            return BadRequest("Invalid username or password.");
         }
 
-        private Task<object> GenerateJwt(string email)
+        private async Task<SessionToken> GenerateJwt(string email)
         {
-            throw new NotImplementedException();
+            var user = await _userManager.FindByEmailAsync(email);
+            
+            if(user == null)
+                user = await _userManager.FindByNameAsync(email);
+            
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_generalConfig.Secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = _generalConfig.Issuer,
+                Audience = _generalConfig.ValidIn.First(),
+                Expires = DateTime.UtcNow.AddHours(_generalConfig.ExpirationHours),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
+            Response.Headers.Add("access-token", token);
+            Response.Headers.Add("client", user.UserName);
+            Response.Headers.Add("uid", user.Email);
+
+            return new SessionToken()
+            {
+                AccessToken = token,
+                Client = user.UserName,
+                UID = user.Email
+            };
         }
     }
 }
--- Backend/StockSimulator.Application/ViewModels/LoginUserViewModel.cs
+++ Backend/StockSimulator.Application/ViewModels/LoginUserViewModel.cs
@@ -4,9 +4,9 @@ namespace StockSimulator.Application.ViewModels
 {
     public class LoginUserViewModel
     {
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage = "The email is an invalid format")]
-        public string Email { get; set; }
+        [Required(ErrorMessage = "UserName is required")]
+        //[EmailAddress(ErrorMessage = "The email is an invalid format")]
+        public string UserName { get; set; }
         [Required(ErrorMessage = "Password is required")]
         [StringLength(20, ErrorMessage = "The field {0} must be between {2} e {1} characters.", MinimumLength = 8)]
         public string Password { get; set; }
