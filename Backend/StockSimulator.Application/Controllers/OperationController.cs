a6e8cf03b27d4649e0d7b04be4ba4f27500b67af
Marcelo Castro <marcelo.castro@sysmap.com.br>
2021-02-19 02:31:12 -0300

    Add new logic to trades and use of memory cache

---
+++ Backend/StockSimulator.Application/Controllers/OperationController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using StockSimulator.Domain.Interfaces.Repository;
+using StockSimulator.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StockSimulator.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+        private readonly IOperationService _operationService;
+        private readonly IMemoryCache _cache;
+
+
+        public OperationController(IOperationService operationService, IMemoryCache cache)
+        {
+            _operationService = operationService;
+            _cache = cache;
+        }
+
+        // GET: api/<OperationController>
+        [HttpGet]
+        public IEnumerable<dynamic> GetOperationsMostNegotiated()
+        {
+            var cacheEntry = _cache.GetOrCreate("MyCacheKey", entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(30);
+                entry.SetPriority(CacheItemPriority.High);
+                return _operationService.GetMostTradedOperations();
+            });
+
+            return cacheEntry;
+            //return _operationService.GetMostTradedOperations();
+        }
+
+    }
+}
---
+++ Backend/StockSimulator.CrossCutting/Logic/TradeOperations.cs
@@ -0,0 +1,33 @@
+﻿using StockSimulator.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockSimulator.CrossCutting.Logic
+{
+    public class TradeOperations
+    {
+        public static IEnumerable<dynamic> GetMostTradedOperations(List<Operation> operations) 
+        {
+            var querybase = (from t in ((from o in operations
+                 where o.OperationDate >= Convert.ToDateTime(Convert.ToDateTime(DateTime.Now)).AddDays(-7) && o.OperationDate <= DateTime.Now
+                 group o by new {o.Symbol } into g 
+                 select new
+                 {
+                     g.Key.Symbol,
+                     Total = g.Count()
+                 }))
+                 orderby t.Total descending
+                 select new
+                 {
+                    Symbol = t.Symbol,
+                    Total = t.Total,
+                    CurrentPrice = operations?.Where(f => f.Symbol.Equals(t.Symbol))?.OrderByDescending(f => f.OperationDate)?.FirstOrDefault()?.CurrentPrice ?? 0M
+                 }).Take(5);
+
+            return querybase;
+
+        }
+    }
+}
