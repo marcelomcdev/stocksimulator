6dfffa0ff3aa986d6fbdf58d5e69a2673d776890
Marcelo Castro <marcelo.castro@sysmap.com.br>
2021-02-18 22:58:24 -0300

    Add migrations

---
+++ Backend/StockSimulator.Domain/Interfaces/Services/IListenerService.cs
@@ -0,0 +1,18 @@
+﻿using StockSimulator.Domain.ValuableObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockSimulator.Domain.Interfaces.Services
+{
+    public interface IListenerService
+    {
+        Task<dynamic> Listen(string url, string filter);
+        Task<dynamic> Listen(string url);
+        void StopListening();
+        public dynamic Item { get; set; }
+        public List<Quote> Items { get; set; }
+
+    }
+}
