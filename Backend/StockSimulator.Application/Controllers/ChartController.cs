7b67e4ff444517bbc6f2c595106a85fba67074db
Marcelo Castro <marcelo.castro@sysmap.com.br>
2021-03-27 16:53:19 -0300

    Add ChartController and SignalR

---
+++ Backend/StockSimulator.Application/Controllers/ChartController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using StockSimulator.Data.DataStorage;
+using StockSimulator.Service.HubConfig;
+using StockSimulator.Service.TimerFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockSimulator.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : Controller
+    {
+        private IHubContext<ChartHub> _hub;
+
+        public ChartController(IHubContext<ChartHub> hub)
+        {
+            _hub = hub;
+        }
+
+        public IActionResult Get()
+        {
+            var timerManager = new TimerManager(()=> _hub.Clients.All.SendAsync("transferchartdata", DataManager.GetData()));
+            return Ok(new { Message = "Request Completed" });
+        }
+    }
+}
---
+++ Backend/StockSimulator.Data/DataStorage/DataManager.cs
@@ -0,0 +1,22 @@
+﻿using StockSimulator.Domain.ValuableObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockSimulator.Data.DataStorage
+{
+    public static class DataManager
+    {
+        public static List<ChartModel> GetData()
+        {
+            var r = new Random();
+            return new List<ChartModel>()
+            {
+                new ChartModel { Data = new List<int> { r.Next(1,40) }, Label = "Data 1" },
+                new ChartModel { Data = new List<int> { r.Next(1,40) }, Label = "Data 2" },
+                new ChartModel { Data = new List<int> { r.Next(1,40) }, Label = "Data 3" },
+                new ChartModel { Data = new List<int> { r.Next(1,40) }, Label = "Data 4" },
+            };
+        }
+    }
+}
