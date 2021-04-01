e46f77c2b42df9daf42530914c4b79604a25dca2
Marcelo Castro <marcelo.castro@sysmap.com.br>
2021-04-01 19:39:05 -0300

    Add validations to cpf and password generated in backend

--- Frontend/src/app/components/register-user/register-user.component.ts
+++ Frontend/src/app/components/register-user/register-user.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { UserService } from './../../services/user.service';
 import { User } from 'src/app/model/user';
 import { Component, OnInit } from '@angular/core';
@@ -11,7 +12,7 @@ export class RegisterUserComponent implements OnInit {
 
   public usuario;
   public mensagem: string;
-  constructor(private userService: UserService) { }
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {
     this.usuario = new User();
@@ -22,11 +23,14 @@ export class RegisterUserComponent implements OnInit {
     .subscribe(
       userJson => { console.log(userJson);
         this.userService.user  = userJson;
+        this.router.navigate(['/']);
        },
       err=> {
         console.log(err);
         if(err.error.errors["Password"] != null)
           this.mensagem = err.error.errors.Password.toString();
+        else if(err.error.errors["Cpf"] != null)
+        this.mensagem = err.error.errors.Cpf.toString();
         else
           this.mensagem = err.error;
       }
